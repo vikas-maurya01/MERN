0e8329d0d543b5f0002cfaa712343a9c25b20546
Vikas Maurya <110195611+vikas-maurya01@users.noreply.github.com>
2025-05-23 17:21:43 +0530

    react.js

--- React.Js/my-app/src/component/AddItem.js
+++ React.Js/my-app/src/component/AddItem.js
@@ -3,16 +3,16 @@ import './AddItem.css';
 function AddItem() {
     return (
         <form className='form'>
-            <div className='product-name'>
+            <div className='add-product-name'>
                 <label id='name'>Name of Product</label>
-                <input type="text" id="name" placeholder="Nmae Of Product"></input>
+                <input type="text" id="name" placeholder="Name Of Product"></input>
             </div>
             <div className='product-date'>
                 <label id='date'>Date of Product</label>
                 <input type="date" id="date" min='2020-01-01' max='2025-12-31' placeholder="date Of Product"></input>
             </div>
-            <div className='submit-btn'>
-                <button type='submit'>Add Item</button>
+            <div >
+                <button className='submit-btn' type='submit'>Add Item</button>
 
             </div>
         </form>
