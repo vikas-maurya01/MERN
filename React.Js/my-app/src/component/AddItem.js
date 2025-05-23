184ea0c016fdb8d9c49f43376aec7f1b8c6322b6
Vikas Maurya <110195611+vikas-maurya01@users.noreply.github.com>
2025-05-23 18:11:21 +0530

    updated App.js

--- React.Js/my-app/src/component/AddItem.js
+++ React.Js/my-app/src/component/AddItem.js
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import './AddItem.css';
 
 function AddItem() {
+
+    const[prodName, setProdName]=useState('')
+    const[prodDate, setProdDate]=useState('')
+
+    function inputHandler(event){
+        // console.log(event.target.value);
+        setProdName(event.target.value);
+    }
+    
+    function changeDate(event){
+        setProdDate(event.target.value);
+
+        console.log(prodDate);
+        console.log(prodName);
+    }
     return (
         <form className='form'>
             <div className='add-product-name'>
                 <label id='name'>Name of Product</label>
-                <input type="text" id="name" placeholder="Name Of Product"></input>
+                <input type="text" onChange={inputHandler} id="name" placeholder="Name Of Product"></input>
             </div>
             <div className='product-date'>
                 <label id='date'>Date of Product</label>
-                <input type="date" id="date" min='2020-01-01' max='2025-12-31' placeholder="date Of Product"></input>
+                <input type="date" onChange={changeDate} id="date" min='2020-01-01' max='2025-12-31' placeholder="date Of Product"></input>
             </div>
             <div >
                 <button className='submit-btn' type='submit'>Add Item</button>
