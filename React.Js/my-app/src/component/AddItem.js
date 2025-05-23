e0398ef97e3c42df2ac909fd7e89120f8099764f
Vikas Maurya <110195611+vikas-maurya01@users.noreply.github.com>
2025-05-23 18:27:54 +0530

    updated

--- React.Js/my-app/src/component/AddItem.js
+++ React.Js/my-app/src/component/AddItem.js
@@ -3,8 +3,8 @@ import './AddItem.css';
 
 function AddItem() {
 
-    const[prodName, setProdName]=useState('')
-    const[prodDate, setProdDate]=useState('')
+    const[newProdName, setProdName]=useState('')
+    const[newprodDate, setProdDate]=useState('')
 
     function inputHandler(event){
         // console.log(event.target.value);
@@ -14,18 +14,32 @@ function AddItem() {
     function changeDate(event){
         setProdDate(event.target.value);
 
-        console.log(prodDate);
-        console.log(prodName);
+        // console.log(prodDate);
+        // console.log(prodName);
     }
+
+    function submitForm(event){
+        event.preventDefault();
+        
+        let prodDetail={
+            prodDate:newprodDate,
+            prodName:newProdName
+        };
+
+        setProdDate("");
+        setProdName("");
+        console.log(prodDetail);
+    }
+
     return (
-        <form className='form'>
+        <form className='form'onSubmit={submitForm} >
             <div className='add-product-name'>
                 <label id='name'>Name of Product</label>
-                <input type="text" onChange={inputHandler} id="name" placeholder="Name Of Product"></input>
+                <input type="text" onChange={inputHandler} value={newProdName} id="name" placeholder="Name Of Product"></input>
             </div>
             <div className='product-date'>
                 <label id='date'>Date of Product</label>
-                <input type="date" onChange={changeDate} id="date" min='2020-01-01' max='2025-12-31' placeholder="date Of Product"></input>
+                <input type="date" onChange={changeDate} id="date" value={newprodDate} min='2020-01-01' max='2025-12-31' placeholder="date Of Product"></input>
             </div>
             <div >
                 <button className='submit-btn' type='submit'>Add Item</button>
